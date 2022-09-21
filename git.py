d8389e7bda58b7ca3b879e1734499c953694d57c
osama-gawish <64300032+osama-gawish@users.noreply.github.com>
2022-09-21 11:35:52 +0200

    On A Call
    #solution for 24.2 Practice, Python Data Structures, sololearn without using while.
    On A Call

--- git.py
+++ git.py
@@ -1,18 +1,21 @@
+#solution for 24.2 Practice, Python Data Structures, sololearn
+#without using while
+
 class CallCenter:
     def __init__(self):
       self.customers = []
 
-    def is_empty(self):
-      return self.customers == []
+#     def is_empty(self):
+#       return self.customers == []
 
     def add(self, x):
       self.customers.insert(0, x)
 
-    def nextt(self):
-      return self.customers.pop()
+#     def nextt(self):
+#       return self.customers.pop()
     
     def name(self):
-        return '{}'.format(self.customers)
+        return ['{}'.format(elm) for elm in self.customers]
            
 
 c = CallCenter()
