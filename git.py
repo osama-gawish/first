bbd163f5d8dd904e224d4fd96a02d58eaa803401
osama-gawish <64300032+osama-gawish@users.noreply.github.com>
2022-09-21 11:28:24 +0200

    Add files via upload

---
+++ git.py
@@ -0,0 +1,34 @@
+class CallCenter:
+    def __init__(self):
+      self.customers = []
+
+    def is_empty(self):
+      return self.customers == []
+
+    def add(self, x):
+      self.customers.insert(0, x)
+
+    def nextt(self):
+      return self.customers.pop()
+    
+    def name(self):
+        return '{}'.format(self.customers)
+           
+
+c = CallCenter()
+
+while True:
+    n = input()
+    if n == 'end':
+        break
+    c.add(n)
+
+time=0
+for i in c.name():
+    if i == 'general':
+        time +=5
+    elif i == 'technical':
+        time +=10 
+
+
+print(time)
